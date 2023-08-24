e18330847a2e253e2e4636fc9e3006debca297a0
Roland Ogunleye <rolandogunleye@gmail.com>
2023-08-24 18:59:15 -0400

    first commit

---
+++ client/src/components/layout/title/index.tsx
@@ -0,0 +1,19 @@
+import { TitleProps, useRouterContext } from "@pankod/refine-core";
+import { Button } from "@pankod/refine-mui";
+import React from "react";
+
+export const Title: React.FC<TitleProps> = ({ collapsed }) => {
+  const { Link } = useRouterContext();
+
+  return (
+    <Button fullWidth variant="text" disableRipple>
+      <Link to="/">
+        {collapsed ? (
+          <img src="" alt="" width="28px" />
+        ) : (
+          <img src="" alt="" width="140px" />
+        )}
+      </Link>
+    </Button>
+  );
+};
